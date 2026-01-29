85fce87c70a1be996b11f72d9af10bcc437589e8
Tom Schierenbeck <tom_sch@uni-bremen.de>
2026-01-29 11:40:35 +0100

    [SemDT] Added sessionmaker utils and an explaantaion on how to set up a db permanently.

---
+++ semantic_digital_twin/src/semantic_digital_twin/orm/exceptions.py
@@ -0,0 +1,10 @@
+from dataclasses import dataclass
+
+from krrood.utils import DataclassException
+
+
+@dataclass
+class DatabaseNotAvailableError(DataclassException):
+
+    def __post_init__(self):
+        self.message = f"Database not available. Check environment variable SEMANTIC_DIGITAL_TWIN_DATABASE_URI."
